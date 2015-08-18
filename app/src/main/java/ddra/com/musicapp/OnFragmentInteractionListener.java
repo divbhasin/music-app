3390490bd541be7def495e436e5bc4f17b77b5a6
dylandao99 <dylandao99@gmail.com>
2015-08-18 16:03:41 -0400

    Revert "Fragmented Exercise Options Layout"
    This reverts commit 4169a86511ab74724f0d6d4429d3817143d0151d.

--- app/src/main/java/ddra/com/musicapp/OnFragmentInteractionListener.java
+++
@@ -1,8 +0,0 @@
-package ddra.com.musicapp;
-
-/**
- * Created by Dylan on 8/17/2015.
- */
-    public interface OnFragmentInteractionListener {
-        public void onFragmentMessage(String TAG, Object data);
-    }
